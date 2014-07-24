adc1589228d74aecef0957ec113da3c5c7f47cbf
Christopher Bartling <chris.bartling@gmail.com>
2014-07-24 14:47:31 -0500

    Now putting orders retrieval on a loop for the demonstration.

--- CircuitBreakerSpike/CircuitBreakerSpike.WebApi/Scripts/Application/app.js
+++ CircuitBreakerSpike/CircuitBreakerSpike.WebApi/Scripts/Application/app.js
@@ -7,20 +7,40 @@ var controller = app.controller('OrderManagementController', function($scope, $l
     this.hasErrors = false;
     this.exceptionThrowingEnabled = false;
     this.numberOfInvocations = 0;
+    this.retrieveOrdersButtonText = 'Start retrieving orders';
+    this.isRetrievingOrders = false;
 
-    this.doRetrieveOrders = function () {
-        $log.info('Retrieving orders...');
+    this.doToggleRetrieveOrders = function () {
+        if (!this.isRetrievingOrders) {
+            $log.info('Starting the retrieval of orders.');
+            this.numberOfInvocations = 0;
+            this.isRetrievingOrders = true;
+            this.retrieveOrdersButtonText = 'Stop retrieving orders';
+            this.executeOrderRetrieval();
+        } else {
+            $log.info('Stopping the retrieval of orders.');
+            this.isRetrievingOrders = false;
+            this.retrieveOrdersButtonText = 'Start retrieving orders';
+        }
+    };
 
-        var promise = $http.get('api/Orders');
-        promise.success($.proxy(function (data) {
-            this.numberOfInvocations += 1;
-            this.hasErrors = false;
-            $log.info('Received orders from server-side: ' + data);
-        }, this));
-        promise.error($.proxy(function() {
-            this.numberOfInvocations += 1;
-            this.hasErrors = true;
-        }, this));
+    this.executeOrderRetrieval = function() {
+        if (this.isRetrievingOrders) {
+            $log.info('Retrieving orders...');
+            var promise = $http.get('api/Orders');
+            promise.success($.proxy(function (data) {
+                this.numberOfInvocations += 1;
+                this.hasErrors = false;
+                $log.info('Received orders from server-side: ' + data);
+            }, this));
+            promise.error($.proxy(function () {
+                this.numberOfInvocations += 1;
+                this.hasErrors = true;
+            }, this));
+
+            var func = $.proxy(function() { this.executeOrderRetrieval(); }, this);
+            setTimeout(func, 2000);
+        }
     };
 
     this.disableExceptionThrowingInRepositories = function() {
