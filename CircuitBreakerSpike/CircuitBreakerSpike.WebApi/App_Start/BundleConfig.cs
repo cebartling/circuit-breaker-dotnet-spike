c3863b9f1763d42e4fcfe42427ae7600a715d419
Christopher Bartling <chris.bartling@gmail.com>
2014-07-24 13:08:40 -0500

    Wiring up JavaScript and CSS.

--- CircuitBreakerSpike/CircuitBreakerSpike.WebApi/App_Start/BundleConfig.cs
+++ CircuitBreakerSpike/CircuitBreakerSpike.WebApi/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace CircuitBreakerSpike.WebApi
 {
@@ -9,35 +8,59 @@ namespace CircuitBreakerSpike.WebApi
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/Vendor/jquery-1.9.0.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                "~/Scripts/Vendor/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/Vendor/jquery.unobtrusive*",
+                "~/Scripts/Vendor/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/Vendor/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap-js").Include(
+                "~/Scripts/Vendor/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/Vendor/angular.js",
+                "~/Scripts/Vendor/angular-animate.js",
+                "~/Scripts/Vendor/angular-cookies.js",
+                "~/Scripts/Vendor/angular-loader.js",
+                "~/Scripts/Vendor/angular-resource.js",
+                "~/Scripts/Vendor/angular-route.js",
+                "~/Scripts/Vendor/angular-sanitize.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/application").Include(
+                "~/Scripts/Application/app.js"
+                ));
+
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
+            bundles.Add(new StyleBundle("~/Content/bootstrap-css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css",
+                "~/Content/Site.css"
+                ));
+
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                "~/Content/themes/base/jquery.ui.core.css",
+                "~/Content/themes/base/jquery.ui.resizable.css",
+                "~/Content/themes/base/jquery.ui.selectable.css",
+                "~/Content/themes/base/jquery.ui.accordion.css",
+                "~/Content/themes/base/jquery.ui.autocomplete.css",
+                "~/Content/themes/base/jquery.ui.button.css",
+                "~/Content/themes/base/jquery.ui.dialog.css",
+                "~/Content/themes/base/jquery.ui.slider.css",
+                "~/Content/themes/base/jquery.ui.tabs.css",
+                "~/Content/themes/base/jquery.ui.datepicker.css",
+                "~/Content/themes/base/jquery.ui.progressbar.css",
+                "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
